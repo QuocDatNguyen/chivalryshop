426fe82314f94e8265a004b542a6e026d53c7163
QuocDatNguyen <quocdat04062003@gmail.com>
2023-06-24 15:22:22 +0700

    new

--- Connection.php
+++ Connection.php
@@ -4,6 +4,6 @@
 ?>
 
 <?php
-    $conn = mysqli_connect('td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'acta6puim8rey1ic', 'jy2si2o20374o3rd','inchmdahflnfxipd')
+    $conn = mysqli_connect('td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'acta6puim8rey1ic', 'zcxtklcwzso0rriz','inchmdahflnfxipd')
         or die("Can not connect database" . mysqli_connect_error());
 ?>
