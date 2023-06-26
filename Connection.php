e6517f4c2368ac85a68259f2d6f7895a89a4a01c
QuocDatNguyen <quocdat04062003@gmail.com>
2023-06-26 20:59:50 +0700

    newcommit

--- Connection.php
+++ Connection.php
@@ -4,6 +4,6 @@
 ?>
 
 <?php
-    $conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'bqr3peptm38e8oru', 'ucxue46fuoiwg1nb','vhr97mgxpnfi5i7z')
+    $conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'ob0d43iutxfq7g4e', 'mobtby77bagi2eni','r4m6581xx6kzqgw6')
         or die("Can not connect database" . mysqli_connect_error());
 ?>
